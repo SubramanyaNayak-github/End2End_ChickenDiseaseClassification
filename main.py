3deaf8c058f367c1fbc6c822a05ee952c716a1f1
SubramanyaNayak <subramanyanayak3@gmail.com>
2024-01-25 20:12:28 +0530

    Base_Mosel(Vgg16 model) is updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ChickenDiseaseClassification import logger
 from ChickenDiseaseClassification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassification.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from ChickenDiseaseClassification.pipeline.stage03_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -27,6 +28,21 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+# ------------------------------------------ Base_Model ------------------------------------ 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/ChickenDiseaseClassification/entity/config_entity.py
+++ src/ChickenDiseaseClassification/entity/config_entity.py
@@ -23,3 +23,20 @@ class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
+
+
+
+# ------------------------------------------ Base_Model ------------------------------------ 
+
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
